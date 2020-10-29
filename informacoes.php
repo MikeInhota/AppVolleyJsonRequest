f6039f3abaa5a9522442f51811b84e71ab65029c
Mike Inhota <mike.batista@etecia.com.br>
2020-10-28 23:50:21 -0300

    adicionando os arquivos do meu BD

--- informacoes.php
+++ informacoes.php
@@ -2,14 +2,18 @@
 $user_name = "etecia";
 $password = "123456";
 $host = "localhost";
-$db_name = "dbUsuario";
+$db_name = "dbfilmes";
+
 $con = mysqli_connect($host,$user_name,$password,$db_name);
-$sql = "select * from tbUsuario where nome like '%maria%';";
-$result = mysqli_query($con,$sql);
+
+$sql = "select * from tbtitulos where nome like '%trix%';";
+
+$result = mysqli_query($con, $sql);
+
 if(mysqli_num_rows($result)>0){
+    $row = mysqli_fetch_assoc($result);
 
-	$row = mysqli_fetch_assoc($result);
-
-	echo json_encode(array("Nome"=>$row["nome"],"Email"=>$row["email"],"Mobile"=>$row["mobile"]));
+    echo json_encode(array("Nome"=>$row["nome"], "Tipo"=>$row["tipo"], "Locado"=>$row["locado"] ));
 }
+
 ?>
